4c675daef1969ff0b359ac811745f1d19c4fa213
Davinia Mendía <ridalma@gmail.com>
2016-07-27 21:11:40 +0200

    Response & api_view, también admin para poder añadir a la vieja usanza

--- cats/urls.py
+++ cats/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from cats import views
 
 urlpatterns = [
     url(r'^cats/$', views.cat_list),
     url(r'^cats/(?P<pk>[0-9]+)/$', views.cat_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- cats/views.py
+++ cats/views.py
@@ -1,21 +1,12 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
+from rest_framework import status
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from cats.models import Cat
 from cats.serializers import CatSerializer
 
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def cat_list(request):
     """
     List all cats, or create a new cat.
@@ -23,38 +14,38 @@ def cat_list(request):
     if request.method == 'GET':
         cats = Cat.objects.all()
         serializer = CatSerializer(cats, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CatSerializer(data=data)
+        serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def cat_detail(request, pk):
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def cat_detail(request, pk, format=None):
     """
     Retrieve, update or delete a cat.
     """
     try:
-        cat = Cat.objects.get(pk=pk)
+        cat = Cat.objects.get(pk=pk, format=None)
     except Cat.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = CatSerializer(cat)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
-        serializer = CatSerializer(cat, data=data)
+        serializer = CatSerializer(cat, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         cat.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
