d54837d5e1d34bc0007ec5e5fc61b19637401d65
Davinia Mendía <ridalma@gmail.com>
2016-07-30 14:06:16 +0200

    Using mixins

--- cats/views.py
+++ cats/views.py
@@ -1,52 +1,35 @@
-from rest_framework import status
-from rest_framework.response import Response
 from cats.models import Cat
 from cats.serializers import CatSerializer
-from rest_framework.views import APIView
-from django.http import Http404
+from rest_framework import mixins
+from rest_framework import generics
 
 
-class CatList(APIView):
-    """
-    List all cats, or create a new one.
-    """
-    def get(self, request, format=None):
-        cats = Cat.objects.all()
-        serializer = CatSerializer(cats, many=True)
-        return Response(serializer.data)
+class CatList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
-    def post(self, request, format=None):
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class CatDetail(APIView):
-    """
-    Retrieve, update or delete a cat instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Cat.objects.get(pk=pk)
-        except Cat.DoesNotExist:
-            raise Http404
+class CatDetail(mixins.RetrieveModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.DestroyModelMixin,
+                            generics.GenericAPIView):
 
-    def get(self, request, pk, format=None):
-        cat = self.get_object(pk)
-        serializer = CatSerializer(cat)
-        return Response(serializer.data)
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
-    def put(self, request, pk, format=None):
-        cat = self.get_object(pk)
-        serializer = CatSerializer(cat, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        cat = self.get_object(pk)
-        cat.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
