64f979b7dbd5ce164faefde8a54f15eccee05ee9
Davinia Mendía <ridalma@gmail.com>
2016-07-30 14:08:49 +0200

    Using generic class-based views

--- cats/views.py
+++ cats/views.py
@@ -1,35 +1,13 @@
 from cats.models import Cat
 from cats.serializers import CatSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class CatList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class CatList(generics.ListCreateAPIView):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class CatDetail(mixins.RetrieveModelMixin,
-                            mixins.UpdateModelMixin,
-                            mixins.DestroyModelMixin,
-                            generics.GenericAPIView):
-
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
